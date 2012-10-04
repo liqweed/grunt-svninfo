f82f4939135bd3820b47a3a56d8d705285d63062
Ophir Radnitz <ophir.r@gmail.com>
2012-10-04 12:50:07 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
-grunt-svn
-=========
+# grunt-svn
 
-Grunt.js plugin that provides easy access to Subversion repository metadata.
+A Grunt.js plugin that provides easy access to Subversion repository metadata.
+
+## Getting Started
+Install this grunt plugin next to your project's [grunt.js gruntfile][getting_started] with: `npm install grunt-svn`
+
+Then add this line to your project's `grunt.js` gruntfile:
+
+```javascript
+grunt.loadNpmTasks('grunt-svn');
+```
+
+[grunt]: http://gruntjs.com/
+[getting_started]: https://github.com/gruntjs/grunt/blob/master/docs/getting_started.md
+
+## Documentation
+_(Coming soon)_
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [grunt][grunt].
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) 2012 Ophir  
+Licensed under the MIT license.
---
+++ tasks/svn.js
@@ -0,0 +1,61 @@
+/*
+ * grunt-svn
+ * https://github.com/liqweed/grunt-svn
+ *
+ * Copyright (c) 2012 Ophir
+ * Licensed under the MIT license.
+ */
+
+module.exports = function(grunt) {
+
+  // Please see the grunt documentation for more information regarding task and
+  // helper creation: https://github.com/gruntjs/grunt/blob/master/docs/toc.md
+
+  // ==========================================================================
+  // TASKS
+  // ==========================================================================
+
+  grunt.registerTask('svn_revision', 'Your task description goes here.', function() {
+	var done = this.async();
+	grunt.utils.spawn({
+		cmd: 'svn',
+		args: ['info']
+	}, function(err, result) {
+		if (err) {
+			grunt.log.error(err);
+			return done(false);
+		}
+		svnInfo = grunt.helper('parse_svninfo', result);
+		grunt.log.write(svnInfo.Revision);
+		done();
+	});
+  });
+  
+  grunt.registerHelper('svninfo', function(done) {
+		var svnInfo = {};
+		grunt.utils.spawn({
+			cmd: 'svn',
+			args: ['info']
+		}, function(err, result) {
+			if (err) {
+				grunt.log.error(err);
+				return done(false);
+			}
+			svnInfo = grunt.helper('parse_svninfo', result);
+			done();
+		});
+		grunt.log.writeln(svnInfo + svnInfo.Revision);
+		return svnInfo;
+   });
+   
+   grunt.registerHelper('parse_svninfo', function(svnInfoAsString) {
+		var svnInfo = {};
+		infoLines = svnInfoAsString.split('\n');
+		for (var i = 0; i< infoLines.length; i++) {
+			var parsedLine = infoLines[i].split(': ', 2);
+			svnInfo[parsedLine[0]] = parsedLine[1];
+		}
+		return svnInfo;
+   });
+
+};
